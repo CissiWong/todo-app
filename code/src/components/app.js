e22f6ff6b252e1572204c9db828cdb8c815f58dd
Cecilia Wong <cissi.wong@gmail.com>
2017-12-21 18:00:45 +0100

    threw out everything and started over - yay!

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,63 +1,10 @@
 import React from "react"
-import ListItem from "./list"
+import Input from "./input"
 
 class App extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      items: [
-        {
-          done: false,
-          text: "Yoga"
-        },
-
-        {
-          done: false,
-          text: "Lunchbox"
-        },
-
-        {
-          done: false,
-          text: "Assignment"
-        },
-
-        {
-          done: false,
-          text: "Group assignment"
-        }
-      ]
-    }
-  }
-
-  findChange = () => {
-    if (this.state.items.done === false) {
-      return false
-    } else {
-      return true
-    }
-  }
-
-  handleChange = () => {
-    this.state.items.find(this.findChange)
-  }
-
   render() {
     return (
-      <div>
-        <form >
-          <input />
-          <button>Submit</button>
-          <ul>
-            {this.state.items.map(item => (
-              <ListItem
-                text={item.text}
-                done={item.done}
-                lunchBox={this.handleChange} />
-            ))}
-          </ul>
-        </form>
-      </div>
+      <Input />
     )
   }
 }
